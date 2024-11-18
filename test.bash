49b0621db80b74ebbfab878dac8c6bc885320e5e
Abelrahman Alhanbali <abdelrahman.alhanbali@gmail.com>
2024-11-18 09:50:05 +0900

    Add a softwer test file

---
+++ test.bash
@@ -0,0 +1,25 @@
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2024 Abdelrahman Alhanbali <abdelrahman.alhanbali@gmail.com>
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+	echo ${1}が違うよ。
+	return=1
+}
+
+return=0
+	### NORMAL INPUT ###
+out=$(seq 1755 | ./Binary_Numbers)
+[ "${out}" = 11011011011 ] || ng "$LINENO"
+	
+### STRANGE INPUT ###
+out=$(echo あ| ./Binary_Numbers)
+[ "$?" = 1 ] || ng "$LINENO"	
+[ "${out}" = "" ] || ng "$LINENO"
+ 
+out=$(echo | ./Binary_Numbers)
+[ "$?" = 1 ] || ng "$LINENO"	
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "$return" = 0 ] && echo OK
+exit $return
